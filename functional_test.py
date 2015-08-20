6b6f584a8d513195a42150d09e06065473c20c08
hongmeiq-usc <hongmeiq@usc.edu>
2015-08-20 16:18:17 -0700

    Chapter 2 done.

--- functional_test.py
+++ functional_test.py
@@ -1,15 +1,49 @@
 from selenium import webdriver
+import unittest
 
-# browser = webdriver.Firefox()
-# open firefox browser on selenium hub
-selenium_hub_url = 'http://192.168.59.103:4444/wd/hub' # will be factored out
-browser = webdriver.Remote(
-	command_executor=selenium_hub_url,
-	desired_capabilities={"browserName": "firefox"})
+class NewVisitorTest(unittest.TestCase):
+	selenium_hub_url = 'http://192.168.59.103:4444/wd/hub' # this variable will be factored out in the future
+	django_url = 'http://192.168.59.103:8000' # this variable will be factored out in the future
 
-# go to django app url (assume django app is up)
-django_url = 'http://192.168.59.103:8000' # will be factored out
-browser.get(django_url)
+	def setUp(self):
+		self.browser = webdriver.Remote(
+			command_executor=self.selenium_hub_url,
+			desired_capabilities={"browserName": "firefox"}) # open firefox browser on selenium hub
+		self.browser.implicitly_wait(3)
+		
+	def tearDown(self):
+		self.browser.quit()	
+		
+	def test_can_start_a_list_and_retrieve_it_later(self):
+		# Edith has heard about a cool new online to-do app. She goes
+		# to check out its homepage
+		self.browser.get(self.django_url) # go to django app url (assume django app is up)
+
+		# She notices the page title and header mention to-do lists
+		self.assertIn('To-Do', self.browser.title)
+		self.fail('Finish the test')
+
+		# She is invited to enter a to-do item straight away
+
+		# She types "Buy peacock feathers" into a text box (Edith's hobby
+		# is tying fly-fishing lures)
+
+		# When she hits enter, the page updates, and now the page lists
+		# "1: Buy peacock feathers" as an item in a to-do list
+
+		# There is still a text box inviting her to add another item. She
+		# enters "Use peacock feathers to make a fly" (Edith is very methodical)
+
+		# The page updates again, and now shows both items on her list
+
+		# Edith wonders whether the site will remember her list. Then she sees
+		# that the site has generated a unique URL for her -- there is some
+		# explanatory text to that effect.
+
+		# She visits that URL - her to-do list is still there.
+
+		# Satisfied, she goes back to sleep
 
 
-assert 'Django' in browser.title
+if __name__ == '__main__': 
+	unittest.main(warnings='ignore')
